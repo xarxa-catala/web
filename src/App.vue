9dd149637b35d8e7b88f4a0fbd81359eb99fe250
Abdelilah El Aissaoui <aeab13@gmail.com>
2021-08-30 03:40:01 +0200

    Improved navbar, added bootstrap support and basic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,71 @@
 <template>
-  <div id="nav">
-    <img id="logo" alt="One Piece Català logo" src="./assets/web_icon.jpg">
-    <div class="spacer"/>
-    <button type="button" class="btn btn-outline-primary">Inici</button>
-    <button type="button" class="btn btn-outline-primary">Sobre nosaltres</button>
-  </div>
-  <router-view class="router"/>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" id="main-navbar">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">One Piece Català</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item" @click="navigateTo('/')">
+            <a class="nav-link"
+              :class="{ active: isCurrentPage('/') }" aria-current="page" href="#">Inici</a>
+          </li>
+          <li class="nav-item" @click="navigateTo('/about')">
+            <a
+              class="nav-link"
+              :class="{ active: isCurrentPage('/about') }"
+              href="#"
+              >Qui som?</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Descàrregues</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <router-view class="router" />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentPage: "/",
+    };
+  },
+  watch: {
+    $route(to, from) {
+      console.log(`navigating from ${from.fullPath} to ${to.fullPath}`);
+      this.currentPage = to.fullPath;
+    },
+  },
+
+  methods: {
+    navigateTo(destination) {
+      console.log(`Navigating to ${destination}`);
+      this.$router.push(destination);
+    },
+    isCurrentPage(page) {
+      var isCurrentPage = this.currentPage === page;
+      console.log(`Is current page ${page}? ${isCurrentPage}`);
+
+      return isCurrentPage;
+    },
+  },
+};
+</script>
+
 
 <style lang="scss">
 #app {
@@ -26,7 +85,7 @@
 }
 
 #logo {
-  height: 100px; 
+  height: 100px;
   width: 100px;
 }
 
@@ -36,5 +95,9 @@
 
 .router {
   margin-top: 100px;
+}
+
+#main-navbar {
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
 }
 </style>
