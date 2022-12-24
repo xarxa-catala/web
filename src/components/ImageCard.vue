abeec71e301fcbc720b6a17375554e15531e6444
AleixSanfe <aleix.sanfeliu@gmail.com>
2022-12-24 16:04:15 +0100

    chore: refactor home page

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,40 +1,128 @@
 <template>
-  <div class="xc-card card" style="width: 18rem">
-    <a :href="linkUrl">
-      <img :src="getImage()" class="xc-card-img card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">{{ title }}</h5>
-        <p class="card-text">
-          <slot />
-        </p>
-      </div>
-    </a>
-  </div>
+  <a
+    class="xc-card"
+    :href="linkUrl"
+    :style="'background-color: ' + backgroundColor + ';'"
+  >
+    <div class="xc-card-img">
+      <img v-if="!!imgUrl" :src="getImage()" alt="..." />
+      <i v-if="!!icon" :class="getIcon()"></i>
+    </div>
+    <h2 class="xc-card-title m-0">{{ title }}</h2>
+    <p class="xc-card-text m-0">
+      <slot />
+    </p>
+  </a>
 </template>
+
 <script>
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 export default {
   name: "ImageCard",
   props: {
     imgUrl: String,
+    icon: String,
     title: String,
     linkUrl: String,
-  },
-  mounted() {
-    console.log(this.title);
+    backgroundColor: String,
   },
   methods: {
     getImage() {
       return require(`../assets/${this.imgUrl}`);
-    }
-  }
+    },
+    getIcon() {
+      return `bi bi-${this.icon}`;
+    },
+  },
 };
 </script>
-<style lang="scss" scoped>
-.xc-card:hover {
-  box-shadow: 0 0 5px 5px rgb(0 0 0 / 0.2);
 
+<style lang="scss" scoped>
+.xc-card {
+  width: 100%;
+  height: 350px;
   text-decoration: none;
   cursor: pointer;
+
+  display: grid;
+  grid-template-rows: 175px 175px;
+  grid-template-columns: 350px 1fr;
+
+  &:hover {
+    & > .xc-card-img {
+      transform: scale(1.1);
+    }
+  }
+
+  & > .xc-card-img {
+    grid-column-start: 1;
+    grid-column-end: 1;
+    grid-row-start: 1;
+    grid-row-end: 3;
+    overflow: hidden;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & > img {
+      max-width: 250px;
+    }
+
+    & i.bi {
+      font-size: 10rem;
+    }
+  }
+
+  & > .xc-card-title {
+    grid-column-start: 2;
+    grid-column-end: 2;
+    grid-row-start: 1;
+    grid-row-end: 1;
+    align-self: end;
+    padding: 0 32px;
+  }
+
+  & > .xc-card-text {
+    grid-column-start: 2;
+    grid-column-end: 2;
+    grid-row-start: 2;
+    grid-row-end: 2;
+    padding: 0 32px;
+  }
+
+  @media (max-width: 992px) {
+    width: 100%;
+    height: 400px;
+    grid-template-rows: 250px 50px 100px;
+    grid-template-columns: 1fr;
+
+    & > .xc-card-img {
+      grid-column-start: 1;
+      grid-column-end: 1;
+      grid-row-start: 1;
+      grid-row-end: 1;
+
+      & > img {
+        max-width: 160px;
+      }
+    }
+
+    & > .xc-card-title {
+      grid-column-start: 1;
+      grid-column-end: 1;
+      grid-row-start: 2;
+      grid-row-end: 2;
+    }
+
+    & > .xc-card-text {
+      grid-column-start: 1;
+      grid-column-end: 1;
+      grid-row-start: 3;
+      grid-row-end: 3;
+    }
+  }
 }
 
 a {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,63 @@
 <template>
-  <div id="xc-home">
-    <img src="../assets/logo.png" />
-    <h4 id="xc-welcome-msg">Benvinguts a One Piece Català</h4>
-    <div id="xc-cards-container">
+  <main>
+    <section id="header" class="container">
+      <div class="d-flex justify-content-around align-items-center">
+        <img class="jolly-roger" :src="randomJollyRogerLeft" />
+        <h1>Benvinguts a One Piece Català</h1>
+        <img class="jolly-roger" :src="randomJollyRogerRight" />
+      </div>
+    </section>
 
-      <ImageCard
-        imgUrl="op-card.webp"
-        title="Anime Online"
-        linkUrl="show-online"
-      >
-        Clica aquí per veure online tots els episodis que hi ha doblats al català.
-      </ImageCard>
-      <ImageCard
-        imgUrl="android-logo.webp"
-        title="Aplicació Android"
-        linkUrl="https://t.me/onepiececatala/101821"
-      >
-        Descarrega't l'app de Xarxa Català i visualitza el contingut de
-        forma nativa al teu dispositiu Android.
-      </ImageCard>
+    <hr class="mb-0" />
 
-      <ImageCard
-        imgUrl="telegram-logo.webp"
-        title="Grup de Telegram"
-        linkUrl="https://t.me/onepiececatala/"
-      >
-        Si voleu parlar amb altres seguidors de One Piece i amb els
-        administradors del Projecte, uniu-vos al nostre grup de Telegram!
-      </ImageCard>
-
-      <ImageCard
-        imgUrl="xc-logo.webp"
-        title="Xarxa Catala"
-        linkUrl="https://xarxacatala.cat"
-      >
-        Clica per descobrir tots els nostres projectes i més informació sobre
-        Xarxa Català.
-      </ImageCard>
-    </div>
-  </div>
+    <section id="cards">
+      <div class="row m-0">
+        <div class="col-12 col-lg-6 p-0">
+          <ImageCard
+            imgUrl="crew.webp"
+            title="Anime Online"
+            linkUrl="show-online"
+            backgroundColor="#ee6666"
+          >
+            Clica aquí per veure online tots els episodis que hi ha doblats al
+            català.
+          </ImageCard>
+        </div>
+        <div class="col-12 col-lg-6 p-0">
+          <ImageCard
+            icon="android2"
+            title="Aplicació Android"
+            linkUrl="https://t.me/onepiececatala/101821"
+            backgroundColor="#9bf5c3"
+          >
+            Descarrega't l'app de Xarxa Català i visualitza el contingut de
+            forma nativa al teu dispositiu Android.
+          </ImageCard>
+        </div>
+        <div class="col-12 col-lg-6 p-0">
+          <ImageCard
+            icon="telegram"
+            title="Grup de Telegram"
+            linkUrl="https://t.me/onepiececatala/"
+            backgroundColor="#35abde"
+          >
+            Parlar amb altres seguidors de One Piece i amb els
+            administradors del Projecte. Uniu-vos al nostre grup de Telegram!
+          </ImageCard>
+        </div>
+        <div class="col-12 col-lg-6 p-0">
+          <ImageCard
+            imgUrl="xc-logo.webp"
+            title="Xarxa Catala"
+            linkUrl="https://xarxacatala.cat"
+            backgroundColor="#f9e1a4"
+          >
+            Descobreix tots els nostres projectes i més informació sobre Xarxa Català.
+          </ImageCard>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -50,45 +69,56 @@ export default {
     ImageCard,
   },
   data() {
-    return {};
+    return {
+      randomImageInterval: null,
+      randomJollyRogerLeft: null,
+      randomJollyRogerRight: null,
+      jollyRogers: [
+        "/jolly_rogers/luffy.webp",
+        "/jolly_rogers/zoro.webp",
+        "/jolly_rogers/nami.webp",
+        "/jolly_rogers/usop.webp",
+        "/jolly_rogers/sanji.webp",
+        "/jolly_rogers/chopper.webp",
+        "/jolly_rogers/robin.webp",
+        "/jolly_rogers/franky.webp",
+        "/jolly_rogers/brook.webp",
+        "/jolly_rogers/jimbe.webp",
+      ],
+    };
   },
-  methods: {},
+  mounted() {
+    this.randomImageInterval = setInterval(() => {
+      this.randomJollyRogerLeft = this.randomizeImage();
+      this.randomJollyRogerRight = this.randomizeImage();
+    }, 2000);
+    this.randomJollyRogerLeft = this.randomizeImage();
+    this.randomJollyRogerRight = this.randomizeImage();
+  },
+  unmounted() {
+    clearInterval(this.randomImageInterval);
+  },
+  methods: {
+    randomizeImage() {
+      const randomNumber = this.getRandomNumber();
+      return this.jollyRogers[randomNumber];
+    },
+    getRandomNumber() {
+      return parseInt(Math.random() * 10);
+    },
+  },
 };
 </script>
 
-
 <style scoped lang="scss">
-#xc-home {
-  width: 100%;
-  height: 100%;
-
-  #xc-welcome-msg {
-    font-weight: 700;
-    margin-top: 24px;
-    margin-bottom: 36px;
-  }
-
-  #xc-cards-container {
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-    justify-content: center;
-  }
-  #xc-cards-container > * {
-    margin: 10px;
-  }
+main {
+  background-color: #bcdffc;
 }
 
-$carousel-height: 400px;
-
-.xc-carousel-inner {
-  width: 100%;
-  max-height: $carousel-height !important;
-  img {
-    max-height: $carousel-height !important;
-    object-fit: cover;
-    object-position: 50% 50%; /* try 20px 10px */
-    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+.jolly-roger {
+  width: 128px;
+  @media (max-width: 992px) {
+    width: 64px;
   }
 }
 </style>
