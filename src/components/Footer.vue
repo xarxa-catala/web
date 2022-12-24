61e03ca69671c4c1ee67ae0a783a76f165a1ccf0
AleixSanfe <aleix.sanfeliu@gmail.com>
2022-12-24 16:02:58 +0100

    feat: create new footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,118 @@
+<template>
+  <footer>
+    <div class="container mt-5 pb-5">
+      <div class="row">
+        <div class="col-12">
+          <h4>Qui som i a on anem?</h4>
+        </div>
+        <div class="col-12 col-lg-6">
+          <p>
+            Tot i que comencem molt filosòfics, no pretenem entrar en termes
+            existencials sinó explicar-vos què és el Projecte One Piece Català.
+          </p>
+          <p>
+            El 2006 es va començar a emetre One Piece al K3, i des de llavors
+            que els administradors i molts seguidors de la sèrie hem estat
+            enganxats a l’obra mestra d’Eiichiro Oda. Molts de nosaltres no
+            sabíem que existia el manga d’aquesta sèrie i no ho vam saber fins
+            fa pocs anys, perquè estàvem conformes amb el que veiem per
+            televisió. Després de reiterades pauses en l’emissió, molts vam
+            començar a llegir informació de les wikis, vam descobrir que hi
+            havia un manga que anava força més endavant que el doblatge en
+            català, i també que es podia veure l’anime sense doblar i subtitulat
+            en castellà per poder seguir la sèrie sense esperar que TV3 comprés
+            més episodis. En aquests temps de descobriment, moltes pàgines van
+            ajudar els fans a entendre aspectes de la trama que no s’entenien, i
+            alhora, a enganxar-nos encara més. Molts dels administradors ens vam
+            quedar meravellats d’aquestes pàgines, i volíem formar part
+            d’aquesta ajuda extra que oferia Internet per a tots els seguidors
+            amb ganes de més.
+          </p>
+        </div>
+        <div class="col-12 col-lg-6">
+          <p>
+            És per això que va néixer el Projecte One Piece Català: una gran
+            comunitat formada tant pels seus administradors com els
+            col·laboradors de la mateixa, per poder donar el bon servei de
+            continguts de la sèrie que vam trobar a Internet quan el vam
+            necessitar i, alhora, el que voldríem haver trobat.
+          </p>
+
+          <p>
+            Tots junts intentem posar grans quantitats de contingut de la sèrie
+            a la disposició de la gent que no coneix One Piece i a tots aquells
+            que en són fans empedreïts. Treballem per tal de facilitar novetats,
+            curiositats i notícies de One Piece a tothom amb certa periodicitat.
+          </p>
+
+          <p>
+            Per gaudir del nostre contingut vam crear el nostre web, on podreu
+            consultar des de notícies fins a qualsevol tipus de curiositat i
+            també els diversos comptes oficials de les xarxes socials que
+            administrem, des d’on informem de nous continguts del Projecte i
+            notícies sobre el manga d’Eiichiro Oda i, en especial, del seu
+            doblatge en català.
+          </p>
+          <p>
+            <b
+              >El 14 de desembre de 2015 vam començar el Projecte des de la
+              nostra pàgina de Facebook, i vam seguir obrint comptes a les
+              xarxes socials fins a inaugurar aquest lloc web, el 5 de maig de
+              2016.</b
+            >
+          </p>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="row">
+        <div class="col-12 col-lg-6 text-center text-lg-start">
+          <h4>Xarxa Català</h4>
+        </div>
+        <div
+          class="col-12 col-lg-6 d-flex justify-content-center justify-content-lg-end"
+        >
+          <a href="https://t.me/onepiececatala">
+            <i class="xc-social-icon bi bi-telegram"></i>
+          </a>
+          <a href="https://twitter.com/CatalaOnePiece">
+            <i class="xc-social-icon bi bi-twitter"></i>
+          </a>
+          <a href="https://www.instagram.com/catonepiece/">
+            <i class="xc-social-icon bi bi-instagram"></i>
+          </a>
+          <a href="https://www.facebook.com/onepiececatala">
+            <i class="xc-social-icon bi bi-facebook"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  width: 100%;
+  height: 100%;
+
+  border-top: 2px solid lightgray;
+}
+
+.xc-social-icon {
+  color: #2c3e50;
+  font-size: 2rem;
+  margin-left: 8px;
+  margin-right: 8px;
+}
+
+a {
+  text-decoration: none;
+
+  &:hover {
+    color: #000000;
+    text-decoration: none;
+    cursor: pointer;
+  }
+}
+</style>
