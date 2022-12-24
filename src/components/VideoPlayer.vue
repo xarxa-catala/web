1d7b3d1355e2cad6963a646434540f4d0dd0acff
AleixSanfe <aleix.sanfeliu@gmail.com>
2022-12-24 16:04:50 +0100

    chore: update video player

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,44 +1,25 @@
 <template>
-  <div>
-    <video-js-player id="main-video-player" :options="videoOptions" />
-  </div>
+  <video v-if="episodeSelected.url.length > 0" controls autoplay>
+    <source :src="episodeSelected.url" />
+  </video>
 </template>
 
 <script>
-import VideoJsPlayer from "@/components/VideoJsPlayer.vue";
-
 export default {
   name: "VideoExample",
-  components: {
-    VideoJsPlayer,
-  },
-  data() {
-    return {
-      videoOptions: {
-        autoplay: true,
-        controls: true,
-        sources: [
-          {
-            src: this.episodeSelected.url,
-            type: "video/mp4",
-          },
-        ],
-      },
-    };
-  },
   props: {
-    episodeSelected: {url: ""}
-  },
-  mount() {
-    console.log("Episode URL is " + this.episodeSelected.url)
+    episodeSelected: { url: "" },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-#main-video-player {
-  background-color: black;
+video {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
+  background-color: black;
 }
 </style>
