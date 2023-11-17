965b796fba8afa0bd3cd240673e19324a05f8280
Abdelilah El Aissaoui <aeab13@gmail.com>
2023-11-17 02:15:10 +0100

    Added support for multiple shows

--- src/config.ts
+++ src/config.ts
@@ -3,15 +3,25 @@ export interface ExternalPage {
     url: string,
 }
 
+export interface AdditionalShow {
+    id: number,
+    name: string,
+}
+
 export const showId = 1;
 export const siteName: string = "Doctor Who Català"
 export const mainShow: string = "Doctor Who Online"
+export const additionalShows: AdditionalShow[] = [
+    { id: 2, name: "Torchwood" },
+    { id: 3, name: "Class" },
+    { id: 5, name: "The Sarah Jane Adventures" },
+]
+export const externalPages: ExternalPage[] = [
+    { displayName: "L'Illa punteria", url: "https://lillapunteria.carrd.co/" },
+    { displayName: "Qui som", url: "https://xarxacatala.cat/" },
+]
 export const mainShowOnline: string = "/images/home/dw_online.jpg"
 export const telegramLink = "https://t.me/+P9WOZ_LD8NFpkcp5" // https://t.me/onepiececatala
 export const twitterLink = "https://twitter.com/dwcatala"
 export const appLink = "https://t.me/c/1070960231/30725" // "https://t.me/onepiececatala/101821"
 export const instagramLink: string | undefined = undefined // https://www.instagram.com/catonepiece/
-export const externalPages: ExternalPage[] = [
-    { displayName: "L'Illa punteria", url: "https://lillapunteria.carrd.co/" },
-    { displayName: "Qui som", url: "https://xarxacatala.cat/" },
-]
