515cfe0887cb9dc6b44bc53542c259227aae19f2
Abdelilah El Aissaoui <aeab13@gmail.com>
2023-11-19 03:23:08 +0100

    Improve episodes list design

--- src/components/EpisodeCover.tsx
+++ src/components/EpisodeCover.tsx
@@ -7,15 +7,15 @@ export function EpisodeCover(
 ) {
     return <div class="relative flex flex-row items-center 
     cursor-pointer h-10 
-    hover:bg-slate-700 hover:border-slate-300 rounded-lg border-transparent border-2
+    hover:bg-slate-800 hover:border-slate-500 rounded-lg border-transparent border-2
     px-1 py-5 w-full
     ">
         <div class="
-        bg-blue-600
+        bg-blue-700
         w-8 h-8
         rounded-full 
         flex items-center justify-center
-        font-semibold ">{ props.index }</div>
+        font-semibold">{ props.index }</div>
         <p class="ms-2">{props.name}</p>
     </div>
 }
