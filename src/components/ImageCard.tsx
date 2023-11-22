2331f651bd7e2c4b654ff032c4eac12c4a39b51e
Abdelilah El Aissaoui <aeab13@gmail.com>
2023-11-22 20:36:41 +0100

    Increased index cards size in mobile

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,9 +1,9 @@
 export default function ImageCard(props: { imgUrl: string, title: string, linkUrl: string, description: string }) {
 
   return <>
-    <div class="rounded-lg w-72 group overflow-clip bg-slate-700">
+    <div class="rounded-lg w-full lg:w-72  group overflow-clip bg-slate-700">
       <a href={props.linkUrl}>
-        <div class="w-full h-36 overflow-clip">
+        <div class="w-full h-auto overflow-clip">
           <img src={props.imgUrl} class="group-hover:scale-110 transition-transform duration-200 w-full h-full" alt="..." />
         </div>
         <div class="p-2 w-full m-1">
